a12162d49d87bdaefec6bfdad8cd7124b756fcae
Michał Blajer <michalblajer@gmail.com>
2020-07-21 13:08:55 +0200

    - code cleaning

--- DataScrappingTool.py
+++ DataScrappingTool.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
@@ -6,7 +6,9 @@ import sqlite3
 from sqlite3.dbapi2 import IntegrityError
 from collections import namedtuple
 import itertools
-#%%
+# %%
+
+
 class unit():
 
     def __init__(self, scrapped_info):
@@ -24,41 +26,43 @@ class unit():
         self.speed = cells[8].text.strip()
         self.growth = cells[9].text.strip()
         self.ai_value = cells[10].text.strip()
-    
+
     def __str__(self):
         to_print = ""
         to_print += 'Name: ' + self.name + '\n'
         to_print += 'Type: ' + self.type + '\n'
         to_print += 'Level: ' + self.level + '\n'
         to_print += 'Attack: ' + self.attack + '\n'
-        to_print += 'Defense: '+ self.defense + '\n'
-        to_print += 'Min. Damage: '+self.min_dmg + '\n'
-        to_print += 'Max. Damage: '+self.max_dmg + '\n'
+        to_print += 'Defense: ' + self.defense + '\n'
+        to_print += 'Min. Damage: ' + self.min_dmg + '\n'
+        to_print += 'Max. Damage: ' + self.max_dmg + '\n'
         to_print += 'Health: ' + self.health + '\n'
-        to_print += 'Speed: '+self.speed + '\n'
+        to_print += 'Speed: ' + self.speed + '\n'
         to_print += 'Growth: ' + self.growth + '\n'
-        to_print += 'AI Value: '+self.ai_value 
+        to_print += 'AI Value: ' + self.ai_value
         return to_print
+
 
 class map_object():
 
     def __init__(self, name, value):
         self.name = name
         self.value = value
-    
+
     def __str__(self):
         to_print = ""
         to_print += 'Name: ' + self.name + '\n'
         to_print += 'Value: ' + self.value + '\n'
         return to_print
-    
+
     def __repr__(self):
         to_print = ""
         to_print += 'Name: ' + self.name + '\n'
         to_print += 'Value: ' + self.value + '\n'
         return to_print
 
-#%%
+
+# %%
 html = urlopen("https://heroes.thelazy.net/index.php/List_of_creatures_(HotA)")
 bs = BeautifulSoup(html.read(), 'lxml')
 
@@ -70,10 +74,12 @@ for data in bs.tr.next_siblings:
 
 # %%
 html = urlopen("https://heroes.thelazy.net//index.php/Template_Editor")
-bs = BeautifulSoup(html.read(),'lxml')
+bs = BeautifulSoup(html.read(), 'lxml')
 
 map_objects = []
-pattern = re.compile(r'''<li>[\w \d.\/_=\"';&%$#<>()]{1,}\n<ul><li>Value: [0-9]{1,}.</li>''', flags=re.MULTILINE)
+pattern = re.compile(
+    r'''<li>[\w \d.\/_=\"';&%$#<>()]{1,}\n<ul><li>Value: [0-9]{1,}.</li>''',
+    flags=re.MULTILINE)
 map_objects_to_parse = pattern.findall(str(bs.contents[1]))
 
 pattern_value = re.compile(r'''Value: [0-9]{3,}''', flags=re.MULTILINE)
@@ -82,63 +88,86 @@ for object in map_objects_to_parse:
 
     value = pattern_value.findall(object)[0].split(' ')[-1]
     name = pattern_name.findall(object)[0][1:-1].strip()
-    map_objects.append(map_object(name,value))
+    map_objects.append(map_object(name, value))
 
 # %%
 html = urlopen("https://heroes.thelazy.net/index.php/List_of_artifacts_(HotA)")
-bs = BeautifulSoup(html.read(),'lxml')
+bs = BeautifulSoup(html.read(), 'lxml')
 
-Artifact = namedtuple('Artifact',['Name','Class'])
+Artifact = namedtuple('Artifact', ['Name', 'Class'])
 artifacts = []
-   
+
 for data in bs.tr.next_siblings:
     if data != '\n':
         art_name = data.find_all('td')[0].find_all('a')[1].text.strip()
         art_class = data.find_all('td')[2].text.strip()
-        art = Artifact(art_name,art_class)
-        artifacts.append(art) 
+        art = Artifact(art_name, art_class)
+        artifacts.append(art)
 
-#%%
-html = urlopen("https://heroes.thelazy.net/index.php/External_dwellings_table_(HotA)")
-bs = BeautifulSoup(html.read(),'lxml')
+# %%
+html = urlopen(
+    "https://heroes.thelazy.net/index.php/External_dwellings_table_(HotA)")
+bs = BeautifulSoup(html.read(), 'lxml')
 
-Dwelling = namedtuple('Dwelling',['Name','Creature','Level','Growth','Type'])
-dwellings =[]
-for data in itertools.islice(bs.tr.next_siblings,2,None):
-    if data !='\n':
+Dwelling = namedtuple(
+    'Dwelling', [
+        'Name', 'Creature', 'Level', 'Growth', 'Type'])
+dwellings = []
+for data in itertools.islice(bs.tr.next_siblings, 2, None):
+    if data != '\n':
         type = data.contents[1].a['title']
         name = data.contents[3].text
         creature = data.contents[7].a.attrs['title']
         level = data.contents[9].text
         growth = data.contents[11].text[1:]
-        dwelling = Dwelling(name,creature,level,growth,type)
+        dwelling = Dwelling(name, creature, level, growth, type)
         dwellings.append(dwelling)
 
-#dwellings.pop(-6) ## del second Golem Factory
-#%%
+# %%
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-#%%
-#cursor.execute('''CREATE TABLE objects (name VARCHAR(20) PRIMARY KEY, value INT)''')
-#cursor.execute('''CREATE TABLE units (name VARCHAR(20) PRIMARY KEY, type VARCHAR(20), level INT, Attack INT, Defense INT, minDamage INT, maxDamage INT, Health INT, Speed INT, Growth INT, Value INT)''')
+# %%
+cursor.execute(
+    '''CREATE TABLE objects (name VARCHAR(20) PRIMARY KEY, value INT)''')
 
-#cursor.execute('''CREATE TABLE artifacts (name VARCHAR(20) PRIMARY KEY, class VARCHAR(20), Value INT)''')
+cursor.execute('''CREATE TABLE units (
+    name VARCHAR(20) PRIMARY KEY, type VARCHAR(20),
+    level INT,
+    Attack INT,
+    Defense INT,
+    minDamage INT,
+    maxDamage INT,
+    Health INT,
+    Speed INT,
+    Growth INT,
+    Value INT)''')
 
-cursor.execute('''CREATE TABLE dwellings (name VARCHAR(20), creature VARCHAR(20), level INT, growth INT, type VARCHAR(20)) ''')
+cursor.execute(
+    '''CREATE TABLE artifacts (
+    name VARCHAR(20) PRIMARY KEY,
+    class VARCHAR(20),
+    Value INT)''')
+
+cursor.execute(
+    '''CREATE TABLE dwellings (
+    name VARCHAR(20),
+    creature VARCHAR(20),
+    level INT,
+    growth INT,
+    type VARCHAR(20))''')
 # %%
 query = 'INSERT INTO objects VALUES (?,?)'
 for object in map_objects:
     name = object.name
     value = int(object.value)
-    #print(object)
     try:
-        cursor.execute(query,(name,value))
+        cursor.execute(query, (name, value))
         print(object.name + " added")
     except IntegrityError:
         print(object.name + " already in database")
 connection.commit()
-#%%
+# %%
 query = 'INSERT INTO units VALUES (?,?,?,?,?,?,?,?,?,?,?)'
 for unit in units:
     name = unit.name
@@ -152,33 +181,53 @@ for unit in units:
     speed = unit.speed
     growth = unit.growth
     value = unit.ai_value
-    cursor.execute(query,(name,type,level,attack,defense,mindmg,maxdmg,health,speed,growth,value))
+    cursor.execute(
+        query,
+        (name,
+         type,
+         level,
+         attack,
+         defense,
+         mindmg,
+         maxdmg,
+         health,
+         speed,
+         growth,
+         value))
 connection.commit()
 
-#%%
+# %%
 query = 'INSERT INTO artifacts VALUES (?,?,?)'
-values = {'Treasure':2000,'Minor':5000,'Major':10000,'Relic':20000}
+values = {'Treasure': 2000, 'Minor': 5000, 'Major': 10000, 'Relic': 20000}
 
 for art in artifacts:
-    cursor.execute(query,(art.Name,art.Class,values[art.Class]))
+    cursor.execute(query, (art.Name, art.Class, values[art.Class]))
 connection.commit()
 
-#%%
+# %%
 query = 'INSERT INTO dwellings VALUES (?,?,?,?,?)'
 
 for dwelling in dwellings:
     try:
-        cursor.execute(query,(dwelling.Name,dwelling.Creature,dwelling.Level,dwelling.Growth,dwelling.Type))
+        cursor.execute(
+            query,
+            (dwelling.Name,
+             dwelling.Creature,
+             dwelling.Level,
+             dwelling.Growth,
+             dwelling.Type))
     except IntegrityError:
         print(dwelling)
 connection.commit()
-#%%
-#cursor.execute('SELECT name from units')
-cursor.execute('SELECT name,value FROM objects WHERE name LIKE ? or name LIKE ? or name LIKE ?',['Pandora%','Spell%','Prison%'])
+# %%
+cursor.execute(
+    '''SELECT name,value
+         FROM objects
+         WHERE name
+         LIKE ? or name LIKE ? or name LIKE ?''', [
+        'Pandora%', 'Spell%', 'Prison%'])
 rows = cursor.fetchall()
 print([name[0] for name in rows])
 # %%
 cursor.close()
 connection.close()
-
-# %%
--- calc.py
+++ calc.py
@@ -1,75 +1,80 @@
 import sqlite3
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout,\
-    QComboBox,QDialog, QVBoxLayout,QGroupBox, QGridLayout, QTextEdit, QLabel,\
-    QRadioButton,QSpinBox, QListWidget, QTabWidget, QSizePolicy, QListWidgetItem
-from PyQt5.Qt import QMimeData
-from PyQt5.QtCore import *
+    QComboBox, QDialog, QVBoxLayout, QGroupBox, QGridLayout, QTextEdit,\
+    QLabel, QRadioButton, QSpinBox, QListWidget, QTabWidget, QSizePolicy,\
+    QListWidgetItem
+
 
 class WidgetGallery(QDialog):
     def __init__(self, parent=None):
-        super(WidgetGallery,self).__init__(parent)
+        super(WidgetGallery, self).__init__(parent)
         self.originalPalette = QApplication.palette()
-        
+
         self.connection = sqlite3.connect('data.db')
         self.cursor = self.connection.cursor()
-        
-        
-        #self.createGuardWidget()
+
         self.createGuardiansStrengthWidget()
         self.createZonesOnMapWidget()
         self.createPandoraWidget()
 
         bottomTabs = QTabWidget()
-        #bottomTabs.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Ignored)
         bottomTabs.addTab(self.PandoraWidget, "Pandora Calculator")
         mainLayout = QGridLayout()
-        mainLayout.addWidget(self.guardiansStrengthWidget,0,0)
-        mainLayout.addWidget(self.zonesOnMapWidget,0,1)
-        mainLayout.addWidget(bottomTabs,1,0,2,2)
-        mainLayout.setRowStretch(2,2)
+        mainLayout.addWidget(self.guardiansStrengthWidget, 0, 0)
+        mainLayout.addWidget(self.zonesOnMapWidget, 0, 1)
+        mainLayout.addWidget(bottomTabs, 1, 0, 2, 2)
+        mainLayout.setRowStretch(2, 2)
         self.setLayout(mainLayout)
         self.totalValue = 0
 
     def createGuardiansStrengthWidget(self):
         self.guardiansStrengthWidget = QGroupBox("Guardians strength:")
 
-        self.globalMonstersStrength = {'Weak':2,'Normal':3,'Strong':4}
+        self.globalMonstersStrength = {'Weak': 2, 'Normal': 3, 'Strong': 4}
         globalLabel = QLabel('Global monster strength:')
         self.globalGuardiansStrength = QComboBox()
-        
         self.globalGuardiansStrength.addItems(self.globalMonstersStrength)
-        #self.globalGuardiansStrength.setCurrentIndex(1)
 
-        self.zoneMonstersStrength = {'Weak':-1,'Average':0,'Strong':1}
+        self.zoneMonstersStrength = {'Weak': -1, 'Average': 0, 'Strong': 1}
         zoneLabel = QLabel('Zone monster strength:')
         self.zoneGuardianStrength = QComboBox()
         self.zoneGuardianStrength.addItems(self.zoneMonstersStrength)
-        
-        self.globalGuardiansStrength.currentTextChanged.connect(self.calculateValue)
-        self.zoneGuardianStrength.currentTextChanged.connect(self.calculateValue)
-        
 
+        self.globalGuardiansStrength.currentTextChanged.connect(
+            self.calculateValue)
+        self.zoneGuardianStrength.currentTextChanged.connect(
+            self.calculateValue)
 
         layout = QGridLayout()
-        layout.addWidget(globalLabel,0,0)
-        layout.addWidget(self.globalGuardiansStrength,0,1)
-        layout.addWidget(zoneLabel,1,0)
-        layout.addWidget(self.zoneGuardianStrength,1,1)
+        layout.addWidget(globalLabel, 0, 0)
+        layout.addWidget(self.globalGuardiansStrength, 0, 1)
+        layout.addWidget(zoneLabel, 1, 0)
+        layout.addWidget(self.zoneGuardianStrength, 1, 1)
         self.guardiansStrengthWidget.setLayout(layout)
-    
+
     def createZonesOnMapWidget(self):
         self.zonesOnMapWidget = QGroupBox("Zones on map:")
 
         zoneTypeLabel = QLabel("Zone type: ")
         self.ZoneType = QComboBox()
-        self.ZoneType.addItems(['Castle','Rampart','Tower','Inferno','Necropolis','Dungeon','Stronghold','Fortress','Conflux','Cove','Neutral'])
+        self.ZoneType.addItems(['Castle',
+                                'Rampart',
+                                'Tower',
+                                'Inferno',
+                                'Necropolis',
+                                'Dungeon',
+                                'Stronghold',
+                                'Fortress',
+                                'Conflux',
+                                'Cove',
+                                'Neutral'])
         self.ZoneType.currentTextChanged.connect(self.chooseDwellings)
 
         currentZonesLabel = QLabel("Same type zones: ")
         self.currentZones = QSpinBox()
         self.currentZones.setValue(1)
         self.currentZones.setMinimum(1)
-       
+
         allZonesLabel = QLabel("All zones:")
         self.allZones = QSpinBox()
         self.allZones.setMinimum(1)
@@ -81,60 +86,70 @@ class WidgetGallery(QDialog):
         self.allZones.valueChanged.connect(self.calculateValue)
 
         layout = QGridLayout()
-        layout.addWidget(zoneTypeLabel,0,0)
-        layout.addWidget(self.ZoneType,0,1)
-        layout.addWidget(currentZonesLabel,1,0)
-        layout.addWidget(self.currentZones,1,1)
-        layout.addWidget(allZonesLabel,2,0)
-        layout.addWidget(self.allZones,2,1)
+        layout.addWidget(zoneTypeLabel, 0, 0)
+        layout.addWidget(self.ZoneType, 0, 1)
+        layout.addWidget(currentZonesLabel, 1, 0)
+        layout.addWidget(self.currentZones, 1, 1)
+        layout.addWidget(allZonesLabel, 2, 0)
+        layout.addWidget(self.allZones, 2, 1)
         self.zonesOnMapWidget.setLayout(layout)
 
     def createPandoraWidget(self):
         self.PandoraWidget = QGroupBox()
 
-        self.cursor.execute('SELECT name, value, growth FROM units')
+        self.cursor.execute('''SELECT name, value, growth
+                                 FROM units''')
         self.units_names = dict()
         for name, value, growth in self.cursor.fetchall():
-            self.units_names[name] = [value,growth]
-        self.cursor.execute('SELECT name, value FROM objects')
+            self.units_names[name] = [value, growth]
+        self.cursor.execute('''SELECT name, value
+                                 FROM objects''')
         self.object_names = dict(self.cursor.fetchall())
-        self.cursor.execute('SELECT name,value FROM artifacts')
+        self.cursor.execute('''SELECT name,value
+                                 FROM artifacts''')
         self.artifacts_names = dict(self.cursor.fetchall())
-        
-        self.cursor.execute('SELECT name,value FROM objects WHERE name LIKE ? or name LIKE ? or name LIKE ?',['Pandora%','Spell%','Prison%'])
+
+        self.cursor.execute(
+            '''SELECT name,value
+                 FROM objects
+                 WHERE name
+                 LIKE ? or name LIKE ? or name LIKE ?''', [
+                'Pandora%', 'Spell%', 'Prison%'])
         self.unknown_objects = dict(self.cursor.fetchall())
 
-        #print(self.objects_to_calculate)
-
         self.units = QComboBox()
-        #self.units_names = [name[0] for name in self.units_names]
         self.units.addItems(sorted(self.units_names.keys()))
-        #units.setContentsMargins(50,50,50,50)
         unitLabel = QLabel("&Guard:")
         unitLabel.setBuddy(self.units)
         self.units.currentTextChanged.connect(self.calculateValue)
 
         self.quantities = QComboBox()
-        self.quantities.addItems(['Few 1-5','Several 5-9','Pack 10-19','Lots 20-49','Horde 50-99','Throng 100-249','Swarm 250-499','Zounds 500-999','Legion 1000-4000'])        
-        
-        
+        self.quantities.addItems(['Few 1-5',
+                                  'Several 5-9',
+                                  'Pack 10-19',
+                                  'Lots 20-49',
+                                  'Horde 50-99',
+                                  'Throng 100-249',
+                                  'Swarm 250-499',
+                                  'Zounds 500-999',
+                                  'Legion 1000-4000'])
+
         self.objects = QComboBox()
-        #self.object_names = [name[0] for name in self.object_names]
         self.objects.addItems(sorted(self.object_names.keys()))
         objectLabel = QLabel("&Objects:")
         objectLabel.setBuddy(self.objects)
 
-
         self.artifacts = QComboBox()
-        #self.artifacts_names = [name[0] for name in self.artifacts_names]
         self.artifacts.addItems(sorted(self.artifacts_names.keys()))
         artifactLabel = QLabel("&Artifacts:")
         artifactLabel.setBuddy(self.artifacts)
 
         self.object_list = QListWidget()
-        self.object_list.itemDoubleClicked.connect(lambda: self.object_list.takeItem(self.object_list.currentRow()))
+        self.object_list.itemDoubleClicked.connect(
+            lambda: self.object_list.takeItem(
+                self.object_list.currentRow()))
         self.object_list.itemDoubleClicked.connect(self.calculateValue)
-        
+
         add_object_button = QPushButton("Add object")
         add_object_button.clicked.connect(self.addObjectToList)
 
@@ -142,34 +157,29 @@ class WidgetGallery(QDialog):
         add_artifact_button.clicked.connect(self.addArtifactToList)
 
         self.dwellings = QComboBox()
-        #self.dwellings_names = [name[0] for name in self.dwellings_names]
-        #self.dwellings.addItems(sorted(self.dwellings_names.keys()))
         self.chooseDwellings()
         dwellingsLabel = QLabel("&Dwellings:")
         dwellingsLabel.setBuddy(self.dwellings)
 
         add_dwelling_button = QPushButton("Add dwelling")
         add_dwelling_button.clicked.connect(self.addDwellingToList)
-        #remove_last_object_button = QPushButton("Remove last")
-        #remove_last_object_button.clicked.connect(self.removeLastObject)
-        
+
         self.calculatedOutcome = QLabel()
         layout = QGridLayout()
-        layout.addWidget(self.units,0,1)
-        layout.addWidget(unitLabel,0,0)
-        layout.addWidget(self.quantities,0,2)
-        layout.addWidget(objectLabel,1,0)
-        layout.addWidget(self.object_list,0,3,4,1)
-        layout.addWidget(self.objects,1,1)
-        layout.addWidget(add_object_button,1,2)
-        layout.addWidget(artifactLabel,2,0)
-        layout.addWidget(self.artifacts,2,1)
-        layout.addWidget(add_artifact_button,2,2)
-        layout.addWidget(dwellingsLabel,3,0)
-        layout.addWidget(self.dwellings,3,1)
-        layout.addWidget(add_dwelling_button,3,2)
-        layout.addWidget(self.calculatedOutcome,0,7,2,2)
-        #layout.addWidget(remove_last_object_button,2,3,1,2)
+        layout.addWidget(self.units, 0, 1)
+        layout.addWidget(unitLabel, 0, 0)
+        layout.addWidget(self.quantities, 0, 2)
+        layout.addWidget(objectLabel, 1, 0)
+        layout.addWidget(self.object_list, 0, 3, 4, 1)
+        layout.addWidget(self.objects, 1, 1)
+        layout.addWidget(add_object_button, 1, 2)
+        layout.addWidget(artifactLabel, 2, 0)
+        layout.addWidget(self.artifacts, 2, 1)
+        layout.addWidget(add_artifact_button, 2, 2)
+        layout.addWidget(dwellingsLabel, 3, 0)
+        layout.addWidget(self.dwellings, 3, 1)
+        layout.addWidget(add_dwelling_button, 3, 2)
+        layout.addWidget(self.calculatedOutcome, 0, 7, 2, 2)
 
         self.PandoraWidget.setLayout(layout)
 
@@ -177,31 +187,20 @@ class WidgetGallery(QDialog):
         pass
 
     def checkValues(self):
-            curr_value = self.currentZones.value()
-            all_value = self.allZones.value()
-            if all_value < curr_value:
-                self.allZones.setValue(curr_value)
+        curr_value = self.currentZones.value()
+        all_value = self.allZones.value()
+        if all_value < curr_value:
+            self.allZones.setValue(curr_value)
 
     def addObjectToList(self):
-        #QTextEdit.setPlainText(self.PandoraWidget.object_list,'Stesa')
-        #self.PandoraWidget.children.object_list.setPlainText('Button clicked')
         obj = self.objects.currentText()
-        #self.objects_stack.append(obj)
-        #self.cursor.execute('SELECT value FROM objects WHERE name = ?',[obj])
-        #value = self.cursor.fetchall()[0][0]
-        #self.totalValue += value
         self.object_list.addItem(obj)
         self.calculateValue()
 
     def addArtifactToList(self):
         art = self.artifacts.currentText()
-        #self.artifacts_stack.append(art)
-        #self.cursor.execute('SELECT value FROM artifacts WHERE name = ?',[art])
-        #value = self.cursor.fetchall()[0][0]
-        #self.totalValue += value
         self.object_list.addItem(art)
         self.calculateValue()
-        #self.printAll()
 
     def addDwellingToList(self):
         dwelling = self.dwellings.currentText()
@@ -209,14 +208,14 @@ class WidgetGallery(QDialog):
         self.calculateValue()
 
     def calculateValue(self):
-        self.totalValue=0
+        self.totalValue = 0
         items = []
         n = self.currentZones.value()
         N = self.allZones.value()
         for x in range(self.object_list.count()):
             items.append(self.object_list.item(x).text())
 
-        objects_and_artifacts = {**self.object_names,**self.artifacts_names}
+        objects_and_artifacts = {**self.object_names, **self.artifacts_names}
         for item in items:
             try:
                 self.totalValue += objects_and_artifacts[item]
@@ -224,9 +223,8 @@ class WidgetGallery(QDialog):
                 unit = self.dwellings_names[item]
                 unit_value = self.units_names[unit][0]
                 unit_growth = self.units_names[unit][1]
-                value = unit_value*((unit_growth)*(1+n/N)+n/2)
+                value = unit_value * ((unit_growth) * (1 + n / N) + n / 2)
                 self.totalValue += value
-        #print('Total value: ' + str(self.totalValue))
         self.calculateGuard()
 
     def calculateGuard(self):
@@ -235,49 +233,44 @@ class WidgetGallery(QDialog):
         global_coeff = self.globalMonstersStrength[global_text]
         zone_text = self.zoneGuardianStrength.currentText()
         zone_coeff = self.zoneMonstersStrength[zone_text]
-        protection_index = global_coeff + zone_coeff 
-        minimal_value_1 = {1:2500,2:1500,3:1000,4:500,5:0}
-        coefficient_1 = {1:0.5,2:0.75,3:1,4:1.5,5:1.5}
-        minimal_value_2 = {1:7500,2:7500,3:7500,4:5000,5:5000}
-        coefficient_2 = {1:0.5,2:0.75,3:1,4:1,5:1.5}
-        #print('zone: '+ zone_text + ' ' + str(zone_coeff))
-        #print('global: ' + global_text + ' '+ str(global_coeff))
-        #print('prot index:' + str(protection_index))
+        protection_index = global_coeff + zone_coeff
+        minimal_value_1 = {1: 2500, 2: 1500, 3: 1000, 4: 500, 5: 0}
+        coefficient_1 = {1: 0.5, 2: 0.75, 3: 1, 4: 1.5, 5: 1.5}
+        minimal_value_2 = {1: 7500, 2: 7500, 3: 7500, 4: 5000, 5: 5000}
+        coefficient_2 = {1: 0.5, 2: 0.75, 3: 1, 4: 1, 5: 1.5}
         part_1 = object_value - minimal_value_1[protection_index]
         if part_1 < 0:
             part_1 = 0
-        #print('min val 1:' + str(minimal_value_1[protection_index]))
-        
+
         part_2 = object_value - minimal_value_2[protection_index]
         if part_2 < 0:
             part_2 = 0
-        #print('min val 2:' + str(minimal_value_2[protection_index]))    
-        
-        total_AI_Value = part_1 * coefficient_1[protection_index] + part_2 * coefficient_2[protection_index]
-        #print('total ai val:' +str(total_AI_Value))
-        
+
+        total_AI_Value = part_1 * coefficient_1[protection_index] + \
+            part_2 * coefficient_2[protection_index]
+
         guardValue = self.units_names[self.units.currentText()][0]
         if total_AI_Value < 2000:
-            #pass
             self.calculatedOutcome.setText('No protection')
         else:
-            quantity = float(total_AI_Value/guardValue)
+            quantity = float(total_AI_Value / guardValue)
             self.calculateGuardRange(quantity)
-            #self.calculatedOutcome.setText(str(quantity))
 
     def chooseDwellings(self):
         type = self.ZoneType.currentText()
-        self.cursor.execute('SELECT name, creature FROM dwellings WHERE type =?',[type])
+        self.cursor.execute(
+            '''SELECT name, creature
+                 FROM dwellings
+                WHERE type =?''', [type])
         self.dwellings_names = dict(self.cursor.fetchall())
         self.dwellings.clear()
         self.dwellings.addItems(self.dwellings_names.keys())
 
     def calculateGuardRange(self, quantity):
         quantity = round(quantity)
-        #text = self.calculatedOutcome.text()
         text = '\nGuard range: '
         if quantity > 4:
-            modifier = int(quantity/4)
+            modifier = int(quantity / 4)
             low = quantity - modifier
             high = quantity + modifier
             text = text + str(low)
@@ -287,8 +280,7 @@ class WidgetGallery(QDialog):
         else:
             text = text + str(quantity)
         self.calculatedOutcome.setText(text)
-        #self.calcuateUnknownObject()
-    
+
     def calcuateUnknownObject(self):
         guardRange = self.quantities.currentText().split(' ')[1]
         guardRange = guardRange.split('-')
@@ -299,6 +291,7 @@ class WidgetGallery(QDialog):
             self.totalValue += int(v)
             self.calculateGuard()
             self.totalValue = value_of_the_list
+
 
 app = QApplication([])
 window = WidgetGallery()
